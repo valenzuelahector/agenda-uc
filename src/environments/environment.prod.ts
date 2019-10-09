d5ee0dccbbc3d8c88f44176fe9e8ac7489e848d8
valenzuelahector <valenzuelat.hector@gmail.com>
2019-10-09 13:04:38 -0300

    Update environment.prod.ts
    Actualización ID PRD

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -10,6 +10,6 @@ export const ENV = {
   baseApi: 'https://apigw.ucchristus.cl',
   servRoute: '/agendaambulatoria-prod',
   profRoute: '/profesionales-prod',
-  idPlanSaludInit: '4c30555e-5ed3-418f-8f54-a91a00ace99b',
-  idCentrosNoDisponibles: ['8ca284cf-482a-4248-bc8f-a92800f90207']
+  idPlanSaludInit: '0b7a577d-6364-4b28-b2ba-a96e00e243ac',
+  idCentrosNoDisponibles: ['52e43c90-8ab7-4e34-afcb-a96f0106bbd1']
 }
