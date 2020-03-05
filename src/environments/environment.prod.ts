9b322d88b78661716e4cc4567e16b08f0117c507
Eduardo Villalobos <veduardoe@gmail.com>
2020-03-04 21:03:28 -0300

    Se establece idCEntroPrioritario

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,9 +7,10 @@ export const environment = {
 };
 
 export const ENV = {
-   baseApi: 'https://apigw.ucchristus.cl',
-  servRoute: '/agendaambulatoria-test',
-  profRoute: '/profesionales-test',
-  idPlanSaludInit: '4c30555e-5ed3-418f-8f54-a91a00ace99b',
-  idCentrosNoDisponibles: ['8ca284cf-482a-4248-bc8f-a92800f90207']
+  baseApi: 'https://apigw.ucchristus.cl',
+  servRoute: '/agendaambulatoria-prod',
+  profRoute: '/profesionales-prod',
+  idPlanSaludInit: '0b7a577d-6364-4b28-b2ba-a96e00e243ac',
+  idCentrosNoDisponibles: [],
+  idCentroPrioritario: '52e43c90-8ab7-4e34-afcb-a96f0106bbd1'
 }
