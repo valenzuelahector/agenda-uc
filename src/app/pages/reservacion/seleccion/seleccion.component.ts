f06919dd8dca2c06a2b09b4e73b85bd1d8096a31
Eduardo Villalobos <veduardoe@gmail.com>
2020-03-13 22:58:02 -0300

    Se corrigen temas en zonas horarias!

--- src/app/pages/reservacion/seleccion/seleccion.component.ts
+++ src/app/pages/reservacion/seleccion/seleccion.component.ts
@@ -19,7 +19,7 @@ import 'moment-timezone';
 export class SeleccionComponent implements OnInit, OnChanges {
 
   @Input() busquedaInicial: any;
-  @Input() reloadBusqueda:number = 0;
+  @Input() reloadBusqueda: number = 0;
   @Output() calendario: EventEmitter<any> = new EventEmitter();
 
   public recursos: any;
@@ -36,11 +36,15 @@ export class SeleccionComponent implements OnInit, OnChanges {
   public displayCalendar: boolean = true;
   public contadorMeses = 1;
   public enableScroll: boolean = false;
+  public compensacion = -180;
   public navigationDate = {
     min: null,
     max: null
   }
-
+  public setNavigationDate = {
+    min: null,
+    max: null
+  }
   constructor(
     public agendaService: AgendaAmbulatoriaService,
     public utils: UtilsService,
@@ -71,7 +75,9 @@ export class SeleccionComponent implements OnInit, OnChanges {
   }
 
   ngOnChanges() {
+
     if (this.busquedaInicial) {
+
       this.navigationDate = { min: null, max: null };
       this.counterLoader = 0;
       this.displayCalendar = true;
@@ -79,13 +85,15 @@ export class SeleccionComponent implements OnInit, OnChanges {
       let today = new Date();
       let min = today;
       let max = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-      max.setHours(max.getHours() + 23);
-      max.setMinutes(max.getMinutes() + 59)
-      max.setSeconds(max.getSeconds() + 59)
+   //   max.setHours(max.getHours() + 23);
+  //    max.setMinutes(max.getMinutes() + 59)
+ //     max.setSeconds(max.getSeconds() + 59)
 
       this.navigationDate['min'] = min;
       this.navigationDate['max'] = max;
-
+      this.setNavigationDate['min'] = new Date(this.utils.toLocalScl(min, this.compensacion, 'YYYY-MM-DD HH:mm:ss'));
+      this.setNavigationDate['max'] = new Date(this.utils.toLocalScl(max, this.compensacion, 'YYYY-MM-DD HH:mm:ss'));
+  
       if (this.busquedaInicial && this.busquedaInicial.especialidad) {
         this.resetCalendario();
         if (this.busquedaInicial.profesional) {
@@ -123,14 +131,14 @@ export class SeleccionComponent implements OnInit, OnChanges {
         min.setMonth(min.getMonth() + 1);
         newMin = new Date(min.getFullYear(), min.getMonth(), 1);
         newMax = new Date(min.getFullYear(), min.getMonth() + 1, 0);
-        newMax.setHours(newMax.getHours() + 23);
-        newMax.setMinutes(newMax.getMinutes() + 59)
-        newMax.setSeconds(newMax.getSeconds() + 59)
+//        newMax.setHours(newMax.getHours() + 23);
+ //       newMax.setMinutes(newMax.getMinutes() + 59)
+  //      newMax.setSeconds(newMax.getSeconds() + 59)
 
         if (this.counterLoader < 3 && !this.busquedaInicial.profesional) {
           this.counterLoader++;
-          this.getRecursos(null, true);
-        }else{
+          await this.getRecursos(null, true);
+        } else {
           this.utils.showProgressBar();
           setTimeout(() => {
             this.utils.hideProgressBar();
@@ -144,9 +152,9 @@ export class SeleccionComponent implements OnInit, OnChanges {
         min.setMonth(min.getMonth() - 1);
         newMin = new Date(min.getFullYear(), min.getMonth(), (this.contadorMeses == 1) ? today.getDate() : 1);
         newMax = new Date(min.getFullYear(), min.getMonth() + 1, 0)
-        newMax.setHours(newMax.getHours() + 23);
-        newMax.setMinutes(newMax.getMinutes() + 59)
-        newMax.setSeconds(newMax.getSeconds() + 59);
+ //       newMax.setHours(newMax.getHours() + 23);
+  //      newMax.setMinutes(newMax.getMinutes() + 59)
+   //     newMax.setSeconds(newMax.getSeconds() + 59);
 
         this.utils.showProgressBar();
         setTimeout(() => {
@@ -158,6 +166,9 @@ export class SeleccionComponent implements OnInit, OnChanges {
 
     this.navigationDate['min'] = newMin;
     this.navigationDate['max'] = newMax;
+    this.setNavigationDate['min'] = new Date(this.utils.toLocalScl(newMin, this.compensacion, 'YYYY-MM-DD HH:mm:ss'));
+    this.setNavigationDate['max'] = new Date(this.utils.toLocalScl(newMax, this.compensacion, 'YYYY-MM-DD HH:mm:ss'));
+    
     this.conciliarDateDisabled();
     this.determinarMesSinCupo()
 
@@ -214,31 +225,41 @@ export class SeleccionComponent implements OnInit, OnChanges {
 
     return new Promise((resolve, reject) => {
 
-      this.fechaHoy = new Date();
-      this.fechaLimite = new Date();
+      var fechaHoy = new Date();
+      var fechaLimite = new Date();
 
-      if(!idProfesional){
-        this.fechaHoy.setDate(this.fechaHoy.getDate() + ( this.counterLoader * this.maxNumDays));
-        this.fechaLimite.setDate(this.fechaHoy.getDate() + ( this.counterLoader * this.maxNumDays) + this.maxNumDays);
-        this.fechaLimite = new Date(this.fechaLimite.getFullYear(), this.fechaLimite.getMonth() + 1, 0);
-      }else{
-        this.fechaLimite = new Date(this.fechaLimite.getFullYear(), this.fechaLimite.getMonth() + 12, 0);
-      }
-      
-      this.fechaLimite.setHours(this.fechaLimite.getHours() + 23);
-      this.fechaLimite.setMinutes(this.fechaLimite.getMinutes() + 59);
-      this.fechaLimite.setSeconds(this.fechaLimite.getSeconds() + 59);
+      if (!idProfesional) {
 
-      if(this.counterLoader > 0 && !idProfesional){
-          this.fechaHoy = new Date(this.fechaLimite.getFullYear(), this.fechaLimite.getMonth() - 2, 1);
+        fechaHoy.setDate(fechaHoy.getDate() + (this.counterLoader * this.maxNumDays));
+        fechaLimite.setDate(fechaHoy.getDate() + (this.counterLoader * this.maxNumDays) + this.maxNumDays);
+
+      } else {
+
+        fechaLimite = new Date(fechaLimite.getFullYear(), fechaLimite.getMonth() + 12, 0);
       }
 
+      fechaHoy = new Date(this.utils.toLocalScl(fechaHoy, this.compensacion, 'YYYY-MM-DD HH:mm:ss'))
+
+      if (this.counterLoader > 0 && !idProfesional) {
+        fechaHoy.setMonth(fechaHoy.getMonth() + 1);
+        fechaHoy.setDate(1)
+        fechaHoy.setHours(0);
+        fechaHoy.setMinutes(0);
+        fechaHoy.setSeconds(0);
+      }
+
+
+      fechaLimite = new Date(this.utils.toLocalScl(fechaLimite, this.compensacion, 'YYYY-MM-DD HH:mm:ss'));
+      fechaLimite = new Date(fechaLimite.getFullYear(), fechaLimite.getMonth() + 1, 0);
+      fechaLimite.setHours(23);
+      fechaLimite.setMinutes(59);
+      fechaLimite.setSeconds(59);
 
       this.agendaService.getRecursos({
         todosCentro: (this.busquedaInicial.centroAtencion.codigo == 'todos') ? true : false,
         idCentro: this.busquedaInicial.centroAtencion.idCentro,
-        fechaInicio: this.utils.toLocalScl(this.fechaHoy),
-        fechaTermino: this.utils.toLocalScl(this.fechaLimite),
+        fechaInicio: this.utils.trDateStr(fechaHoy, null, this.compensacion),
+        fechaTermino: this.utils.trDateStr(fechaLimite, null, this.compensacion),
         idServicio: this.busquedaInicial.especialidad.idServicio,
         idPlanSalud: ENV.idPlanSaludInit,
         idProfesional: idProfesional
@@ -265,7 +286,7 @@ export class SeleccionComponent implements OnInit, OnChanges {
 
           this.enableScroll = true;
           let listRe = this.orderPipe.transform(data['listaRecursos'], 'proximaFechaEpoch');
-          this.conciliarDataRecursos(listRe, this.fechaHoy, this.fechaLimite);
+          this.conciliarDataRecursos(listRe, fechaHoy, fechaLimite);
         }
 
         this.enableScroll = true;
@@ -288,16 +309,18 @@ export class SeleccionComponent implements OnInit, OnChanges {
     this.recursos.forEach((val, key) => {
       let dateDisabled = this.recursos[key]['datesToHighlight']['dates'];
       this.recursos[key]['datesToHighlight']['dates'] = dateDisabled;
-      this.recursos[key]['datesToHighlight']['dateClass'] = this.dateClass(dateDisabled);
+      this.recursos[key]['datesToHighlight']['dateClass'] = this.dateClass(dateDisabled, val['compensacion']);
+
     })
   }
 
   conciliarDataRecursos(recursosProcesados, desde, hasta): void {
 
     recursosProcesados.forEach((valRe, keyRe) => {
-      console.log(valRe['datesToHighlight'])
+
       var foundInRe = false;
       this.recursos.forEach((valRee, keyRee) => {
+
         if (valRe['id'] == valRee['id']) {
           foundInRe = true;
           this.recursos[keyRee]['cupos'] = valRee['cupos'].concat(valRe['cupos'])
@@ -316,9 +339,8 @@ export class SeleccionComponent implements OnInit, OnChanges {
               dateDisabled.push(key + "T12:00:00.000Z")
             }
           })
-
           this.recursos[keyRee]['datesToHighlight']['dates'] = dateDisabled;
-          this.recursos[keyRee]['datesToHighlight']['dateClass'] = this.dateClass(dateDisabled);
+          this.recursos[keyRee]['datesToHighlight']['dateClass'] = this.dateClass(dateDisabled, valRee['compensacion']);
         }
       })
 
@@ -334,11 +356,12 @@ export class SeleccionComponent implements OnInit, OnChanges {
 
   }
 
-  dateClass(datesDs) {
+  dateClass(datesDs, compensacion) {
     return (date: Date): MatCalendarCellCssClasses => {
-      let datesDisabled = JSON.parse(JSON.stringify(datesDs))
+      let datesDisabled = JSON.parse(JSON.stringify(datesDs));
+
       const highlightDate = datesDisabled
-        .map(strDate => new Date(strDate))
+        .map(strDate => new Date(this.utils.toLocalScl(strDate, this.compensacion, 'YYYY-MM-DD HH:mm:ss')))
         .some(d => d.getDate() === date.getDate() && d.getMonth() === date.getMonth() && d.getFullYear() === date.getFullYear());
 
       return highlightDate ? 'day-disabled' : '';
@@ -362,7 +385,7 @@ export class SeleccionComponent implements OnInit, OnChanges {
   }
 
   onSelect(event, i) {
-
+    console.log(event)
     this.selectedDate[i] = event;
     let fechaDisSel = this.utils.trDateStr(event, 'json');
     let idxFecha = fechaDisSel['year'] + "-" + fechaDisSel['month'] + '-' + fechaDisSel['day'];
@@ -389,10 +412,9 @@ export class SeleccionComponent implements OnInit, OnChanges {
 
   crearCalendario(dataRecurso: any, centros: any, disponibilidades: any, recursos: any) {
 
-    let compensacion = dataRecurso['cupos'].length > 0 ? dataRecurso['cupos'][0]['compensacion'] : -180;
+    this.compensacion = dataRecurso['cupos'].length > 0 ? dataRecurso['cupos'][0]['compensacion'] : -180;
     let fecha = new Date();
     let f = null;
-    console.log(compensacion)
 
     dataRecurso['fechasDisponibles'] = {};
     dataRecurso['listaCentrosIdCorto'] = {};
@@ -415,10 +437,10 @@ export class SeleccionComponent implements OnInit, OnChanges {
     for (let day = 0; day <= 400; day++) {
 
       if (day == 0) {
-        f = this.utils.toLocalScl(fecha, compensacion);
+        f = this.utils.toLocalScl(fecha, this.compensacion);
       } else {
         fecha.setDate(fecha.getDate() + 1);
-        f = this.utils.toLocalScl(fecha, compensacion);
+        f = this.utils.toLocalScl(fecha, this.compensacion);
       }
 
       f = this.utils.toStringDateJson(f);
@@ -426,7 +448,7 @@ export class SeleccionComponent implements OnInit, OnChanges {
       dataRecurso['cupos'].forEach((val, key) => {
 
         let fechaEpoch = new Date(val['horaEpoch'] * 1000);
-        let u:any = this.utils.toLocalScl(fechaEpoch, compensacion);
+        let u: any = this.utils.toLocalScl(fechaEpoch, this.compensacion);
         u = this.utils.toStringDateJson(u);
         val['idTipoCita'] = this.getTipoCita(val['tiposDeCita'][0]);
         val['fechaHora'] = fechaEpoch;
@@ -443,6 +465,7 @@ export class SeleccionComponent implements OnInit, OnChanges {
     }
 
     dataRecurso['datesToHighlight'] = { dates: [], displayed: false, dateClass: null };
+
     let proximaFecha: boolean = false;
     let datesDisabled = [];
 
@@ -453,11 +476,8 @@ export class SeleccionComponent implements OnInit, OnChanges {
         let count = 0;
         dataRecurso['fechasDisponibles'][key].forEach((valDx, keyDx) => {
           if (count == 0) {
-            dataRecurso['proximaFecha'] = new Date(valDx['horaEpoch'] * 1000) ;
-            let k:string = this.utils.toLocalScl(dataRecurso['proximaFecha'], compensacion, 'YYYY-MM-DD HH:mm:ss');
-            dataRecurso['proximaFechaDate'] = new Date(k);
+            dataRecurso['proximaFecha'] = new Date(valDx['horaEpoch'] * 1000);
             dataRecurso['proximaFechaEpoch'] = valDx['horaEpoch'];
-            dataRecurso['compensacion'] = compensacion;
             proximaFecha = true;
           }
           count++;
@@ -467,7 +487,7 @@ export class SeleccionComponent implements OnInit, OnChanges {
 
     dataRecurso['datesToHighlight']['displayed'] = true;
     dataRecurso['datesToHighlight']['dates'] = datesDisabled;
-    dataRecurso['datesToHighlight']['dateClass'] = this.dateClass(datesDisabled);
+    dataRecurso['datesToHighlight']['dateClass'] = this.dateClass(datesDisabled, this.compensacion);
 
     return dataRecurso;
   }
