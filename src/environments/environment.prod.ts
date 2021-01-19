a22e7c514894b44c7598ee51c207cbaad167865c
Eduardo Villalobos <veduardoe@gmail.coms>
2021-01-19 10:41:33 -0300

    Fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -7,13 +7,25 @@ export const environment = {
 };
 
 export const ENV = {
-   baseApi: 'https://apigw.ucchristus.cl',
-   servRoute: '/agendaambulatoria-prod',
-   profRoute: '/profesionales-prod',
-   idPlanSaludInit: '0b7a577d-6364-4b28-b2ba-a96e00e243ac',
-   idCentrosNoDisponibles: [],
-   idCentroPrioritario: '52e43c90-8ab7-4e34-afcb-a96f0106bbd1',
-   mensajeSinCupos : '<h5>Sin disponibilidad en agendamiento web. En caso de requerir atención presencial para vacunación por campaña influenza ministerial, ésta será por orden de llegada.</h5>'
+    baseApi: 'https://apigw.ucchristus.cl',
+    servRoute: '/agendaambulatoria-pre',
+    profRoute: '/profesionales-pre',
+    idPlanSaludInit: '0b7a577d-6364-4b28-b2ba-a96e00e243ac',
+    idCentrosNoDisponibles: [],
+    idCentroPrioritario: '52e43c90-8ab7-4e34-afcb-a96f0106bbd1',
+    mensajeSinCupos : '<h5>Sin disponibilidad en agendamiento web. En caso de requerir atención presencial para vacunación por campaña influenza ministerial, ésta será por orden de llegada.</h5>',
+    idExamenProcedimiento: 'b2461ac2-9c50-4724-9676-a91a00a91a02',
+    idLaboratorioClinico: 'baa46005-a0ff-4715-a074-ac4601235504',
+    areaConsultaMedica: {
+      id: '2a8202d9-1ebd-4f2e-a359-a91a00a91a02',
+      nombre: 'Consultas'
+    },
+    donacionBancoDeSangre: {
+      idEspecialidad: '2ae24ffb-8c57-4943-adcd-abd400f8271c',
+      idServicio : '02560c8e-0c28-4e88-9eb4-abd400f8b282'
+    },
+    idRegion: '6bad9b25-d5df-4565-b5fe-a6f701444053'
+
 }
 
 
